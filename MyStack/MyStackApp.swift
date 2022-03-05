5201fb0096f7a6f5b3045a82dfd11eca0dbd402a
박힘찬 <himchan05@naver.com>
2022-03-05 23:47:52 +0900

    Initial Commit

---
+++ MyStack/MyStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStackApp.swift
+//  MyStack
+//
+//  Created by 박힘찬 on 2022/03/05.
+//
+
+import SwiftUI
+
+@main
+struct MyStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
